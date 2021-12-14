42af7fa39898ea73371bdfcabe77330a2635ce1d
benzkittisak <6206021621082@fitm.kmutnb.ac.th>
2021-12-14 11:04:03 +0700

    ทำ test ปุ่มกดของตัว counter button

---
+++ src/components/CounterButton.test.js
@@ -0,0 +1,25 @@
+import { shallow , mount , render } from 'enzyme'; 
+import CounterButton from './CounterButton';
+
+describe('<CounterButton/>', () => {
+    test('expect to render Card component', () => {
+        const mockColor = 'red';
+        const wrapper = shallow(<CounterButton color={mockColor}/>)
+        expect(wrapper).toMatchSnapshot();
+    })
+    
+    test('corrently increments the counter', () => {
+        const mockColor = 'red';
+        const wrapper = shallow(<CounterButton color={mockColor}/>)
+        
+        wrapper.find('button#counter').simulate('click')
+        expect(wrapper.state()).toEqual({count:2})
+
+        wrapper.find('button#counter').simulate('keypress')
+        expect(wrapper.state()).toEqual({count:2})
+
+        expect(wrapper.props().color).toEqual('red')
+
+    })
+    
+});
